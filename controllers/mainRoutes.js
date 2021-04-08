bcc8ba93c0c451f3cdfa1b0480f95a5d7f661ece
Drissa Bagate <lemanou7@yahoo.fr>
2021-04-07 23:10:25 -0600

    backend: added db schemas,handlebars, models and server settings

--- controllers/mainRoutes.js
+++ controllers/mainRoutes.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const path = require('path');
-
 // This is the 'get' route 
 router.get('/', async (req, res) => {
+  // Here, index.html is rendered
+  res.render('all')
+});
+
+// This is the 'get' route 
+router.get('/html', async (req, res) => {
   // Here, index.html is rendered
   res.sendFile(path.join(__dirname, '../views/index.html'));
 });
---
+++ models/index.js
@@ -0,0 +1,4 @@
+const User = require('./User');
+const Cryptos = require('./cryptos')
+
+module.exports = { User,Cryptos };
