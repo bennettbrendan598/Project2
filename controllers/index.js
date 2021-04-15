e1d11acaf97ec3e6bc249de05155800aaa61a563
Drissa Bagate <lemanou7@yahoo.fr>
2021-04-14 21:31:57 -0600

    Added images, login and styles

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
+const profileRoutes = require('./profile-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-
+router.use('/profile', profileRoutes);
 module.exports = router;
