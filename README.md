d368b42d2b72be7d50673ca0259636fa9a2f57fe
Drissa Bagate <lemanou7@yahoo.fr>
2021-04-16 04:29:54 -0600

    changed the whole directory, made the Api fetch work but need to be style, made the db synced

--- README.md
+++
@@ -1,1 +0,0 @@
-#This is a cryptocurrency project
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,38 @@
 const router = require('express').Router();
-const { User, Crypto } = require('../models');
+const { Project, User } = require('../models');
 const withAuth = require('../utils/auth');
+global.fetch = require("node-fetch");
+ 
+ 
 
-//Home page route
 router.get('/', async (req, res) => {
+ 
   
+    // Pass serialized data and session flag into template
     res.render('homepage');
   
+});
+
+router.get('/project/:id', async (req, res) => {
+  try {
+    const projectData = await Project.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const project = projectData.get({ plain: true });
+
+    res.render('project', {
+      ...project,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // Use withAuth middleware to prevent access to route
@@ -15,7 +41,7 @@ router.get('/profile', withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Crypto }],
+      include: [{ model: Project }],
     });
 
     const user = userData.get({ plain: true });
@@ -29,23 +55,14 @@ router.get('/profile', withAuth, async (req, res) => {
   }
 });
 
-//Contact route
-router.get('/contact', (req, res) => {
-  res.render("contact") 
-})
-
-
-// Login route
 router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect to the homepage
+  // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
     res.redirect('/profile');
     return;
   }
-  // Otherwise, render the 'login' template
+
   res.render('login');
 });
-
-
 
 module.exports = router;
