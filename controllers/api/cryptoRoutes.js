02c036cd8405183b80fe9c198284131e4dc7ecf0
Drissa Bagate <lemanou7@yahoo.fr>
2021-04-16 18:55:40 -0600

    added sign in and sign out and Api but still needs to display user profile

--- controllers/api/cryptoRoutes.js
+++ controllers/api/cryptoRoutes.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Crypto } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
   try {
-    const newProject = await Project.create({
+    const newPortfolio = await Crypto.create({
       ...req.body,
       user_id: req.session.user_id,
     });
 
-    res.status(200).json(newProject);
+    res.status(200).json(newPortfolio);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -17,7 +17,7 @@ router.post('/', withAuth, async (req, res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
   try {
-    const projectData = await Project.destroy({
+    const projectData = await Crypto.destroy({
       where: {
         id: req.params.id,
         user_id: req.session.user_id,
